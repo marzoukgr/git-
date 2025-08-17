df0172f8bb1cbecbc8e7fb1d631ef896aeadc5c3
marzouk <marzoukgahrar103@gmail.com>
2025-08-17 16:20:00 +0100

    ajoute un fichier

---
+++ actuel.php
@@ -0,0 +1,16 @@
+<?php require_once 'isconnect.php'?>
+<link rel="stylesheet" href="style.css">
+<section class="actuel">
+   
+    <nav>
+        <a href="index">maison</a>
+        <a href="actuel/chambre_nettoyer.php">chambre a nettoyer</a>
+        <a href="actuel/notification.php" class="active">notification</a>
+        <a href="actuel/reservation_ahjourdui.php">reservation du jour</a>
+    </nav>
+</section>
+<?php require_once 'includes/footer.php'?>
+    
+        
+    
+   
